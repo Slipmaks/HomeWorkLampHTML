c187c24a6ec7f2457fbfa7ff9ad5985a1f9f3a49
Slipmaks <slipmaks@gmail.com>
2022-01-23 15:17:49 +0200

    just init and add project

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+
+function toggleBtn() {
+  btn.classList.toggle("active-toggle");
+  light.classList.toggle("light-on");
+}

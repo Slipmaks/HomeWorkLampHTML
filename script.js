4ecc08d4d5dc3fdfa97e49bc7345ab39e3b72637
Slipmkas <slipmaks@gmail.com>
2022-01-27 10:38:40 +0200

    final fix

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let btn = document.getElementById("btn");
 let light = document.getElementById("light");
+let offCanvas = document.getElementById("off-canvas");
 
 function toggleBtn() {
   btn.classList.toggle("active-toggle");
   light.classList.toggle("light-on");
+}
+function toggleCanvas() {
+  offCanvas.style.width = "250px";
+}
+
+function closeOffCanvas() {
+  offCanvas.style.width = "0px";
 }
